d47f77dbea5a0ed457d8e492b36b56c503ae60ed
xternalz <jason7fd@gmail.com>
2017-06-29 14:18:06 +0800

    multiscale

--- torchvision/datasets/__init__.py
+++ torchvision/datasets/__init__.py
@@ -1,7 +1,7 @@
 from .lsun import LSUN, LSUNClass
 from .folder import ImageFolder
 from .coco import CocoCaptions, CocoDetection
-from .cifar import CIFAR10, CIFAR100
+from .cifar import CIFAR10, CIFAR100, CIFAR10Z, CIFAR100Z
 from .stl10 import STL10
 from .mnist import MNIST
 from .svhn import SVHN
@@ -11,4 +11,5 @@ __all__ = ('LSUN', 'LSUNClass',
            'ImageFolder',
            'CocoCaptions', 'CocoDetection',
            'CIFAR10', 'CIFAR100',
+           'CIFAR10Z', 'CIFAR100Z',
            'MNIST', 'STL10', 'SVHN', 'PhotoTour')
